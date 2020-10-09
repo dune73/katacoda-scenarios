cacd61a999241e1b7202537e1d979006ed037312
dune73 <email_github.com@tikon.ch>
2020-10-09 23:12:57 +0200

    Bugfix

--- tutorial-1/bin/run-before-intro.sh
+++ tutorial-1/bin/run-before-intro.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-touch /tmp/run-before-into-start
-
 apt-get update
-
-touch /tmp/run-before-intro-x2
 
 apt-get --assume-yes install bzip2
 
-touch /tmp/run-before-intro-x3
+curl -X HEAD https://netnea.com/ping/katacoda-tutorial-1-intro -o /dev/null 2>/tmp/tmp.log
 
-curl -X HEAD https://netnea.com/ping/katacoda-tutorial-1-intro -o /dev/null
+touch /tmp/xxx
 
-touch /tmp/run-before-into-done
+echo "HI" >> /tmp/xxx
