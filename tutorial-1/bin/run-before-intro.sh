517eb3e3c6619527fa1ffc3211dcdc187b0d759e
dune73 <email_github.com@tikon.ch>
2020-10-12 11:27:04 +0200

    bugfix

--- tutorial-1/bin/run-before-intro.sh
+++ tutorial-1/bin/run-before-intro.sh
@@ -6,5 +6,9 @@ apt-get --assume-yes install bzip2 wget
 
 curl -X HEAD https://netnea.com/ping/katacoda-tutorial-1-intro -o /dev/null 2>/tmp/tmp.log
 
-# chmod +x when copying assets does not work. Doing this by hand
+# chmod +x when copying assets does not work. Doing this by hand.
+# the asset has not been copied when this script here runs. We
+# prepare a chmodded file and the +x will persist then the asset 
+# is copied
+touch /usr/local/bin/scenario-status
 chmod +x /usr/local/bin/scenario-status
