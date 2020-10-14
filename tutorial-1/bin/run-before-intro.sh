770e2b3fcb89431b7316b6527f459ef881e079e8
dune73 <email_github.com@tikon.ch>
2020-10-14 15:34:34 +0200

    Transformed scenario-status data into JSON format

--- tutorial-1/bin/run-before-intro.sh
+++ tutorial-1/bin/run-before-intro.sh
@@ -7,7 +7,4 @@ apt-get --assume-yes install bzip2 wget
 curl -X HEAD https://netnea.com/ping/katacoda-tutorial-1-intro -o /dev/null 2>/tmp/tmp.log
 
 # chmod +x when copying assets does not work. Doing this by hand.
-# the asset has not been copied when this script here runs. We
-# prepare a chmodded file and the +x will persist then the asset 
-# is copied
 chmod +x /usr/local/bin/scenario-status
