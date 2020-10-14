c92e42774925d663c53207383b224cb6d3fc9b3d
dune73 <email_github.com@tikon.ch>
2020-10-14 15:59:25 +0200

    bugfix

--- tutorial-1/bin/run-before-intro.sh
+++ tutorial-1/bin/run-before-intro.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
+# chmod +x when copying assets does not work. Doing this by hand.
+chmod +x /usr/local/bin/scenario-status
+
 apt-get update
 
 apt-get --assume-yes install bzip2 wget
 
 curl -X HEAD https://netnea.com/ping/katacoda-tutorial-1-intro -o /dev/null 2>/tmp/tmp.log
 
-# chmod +x when copying assets does not work. Doing this by hand.
-# File is potentially not yet present. Making sure it is. Cp will retain permissions
-touch /usr/local/bin/scenario-status
-chmod +x /usr/local/bin/scenario-status
