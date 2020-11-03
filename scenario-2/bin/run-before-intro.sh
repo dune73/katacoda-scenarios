466891f2cd91e89378d8a6d3ccdfc1f9954a7374
dune73 <email_github.com@tikon.ch>
2020-11-03 10:38:20 +0100

    Debugging init script

--- scenario-2/bin/run-before-intro.sh
+++ scenario-2/bin/run-before-intro.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-curl -X HEAD https://netnea.com/ping/katacoda-tutorial-2-intro -o /dev/null 2>/tmp/tmp.log
-
 apt-get update
 
 apt-get --assume-yes install bzip2 wget jq
@@ -14,11 +12,13 @@ echo "Downloading now" >> /tmp/tmp.log
 wget https://netnea.com/files/apache-compiled-2.4.46.tar.bz2 2>>/tmp/tmp.log
 echo $? >> /tmp/tmp.log
 echo "Downloaded" >> /tmp/tmp.log
-tar xvjf https://netnea.com/files/apache-compiled-2.4.46.tar.bz2 2>>/tmp/tmp.log
+tar xvjf apache-compiled-2.4.46.tar.bz2 2>>/tmp/tmp.log
 ln -s /opt/apache-2.4.46 /apache
 #FIXME: sudo chown
 
 # chmod +x when copying assets does not work. Doing this by hand.
 chmod +x /usr/local/bin/scenario-status
 
+
+curl -X HEAD https://netnea.com/ping/katacoda-tutorial-2-intro -o /dev/null 2>/tmp/tmp.log
 
