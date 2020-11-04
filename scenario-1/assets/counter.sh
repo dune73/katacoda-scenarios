479999925735fcd0d395dadcd1496fc9b6300455
dune73 <email_github.com@tikon.ch>
2020-11-04 11:10:40 +0100

    update

--- scenario-1/assets/counter.sh
+++ scenario-1/assets/counter.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+COUNTER_FILE="/usr/local/share/step-counter.txt"
+if [ ! -f "$COUNTER_FILE" ]; then
+  echo "0" > $COUNTER_FILE
+  echo "Counter file $COUNTER_FILE created."
+fi
+COUNTER=`head -n 1 $COUNTER_FILE`
+echo "Step was $COUNTER"
+COUNTER=`expr $COUNTER + 1`
+echo "Step advanced to $COUNTER"
+echo "$COUNTER" > $COUNTER_FILE
+
+exit 0
+
+
 JSON=/usr/local/etc/scenario-status.json
 
 STEPCOUNTERFILE=/usr/local/share/step-counter.txt
