48a51880bbe2840608d0fcc35fc088160cc38507
dune73 <email_github.com@tikon.ch>
2020-11-04 14:23:51 +0100

    update

--- scenario-2/bin/run-before-intro.sh
+++ scenario-2/bin/run-before-intro.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+# chmod +x when copying assets does not work. Doing this by hand.
+chmod +x /usr/local/bin/scenario-status
+
 apt-get update
 
-apt-get --assume-yes install bzip2 wget jq
+# Most of these packages are installed, but sometimes, they are not
+apt-get --assume-yes install bzip2 wget jq sudo
 
-# Installed env as prepared in scenario 1
+# Get env as prepared in scenario 1
 cd /
 wget https://netnea.com/files/apache-compiled-2.4.46.tar.bz2
 tar xvjf apache-compiled-2.4.46.tar.bz2
@@ -13,9 +17,5 @@ ln -s /opt/apache-2.4.46 /apache
 
 echo "Env downloaded and installed" >> /tmp/tmp.log
 
-# chmod +x when copying assets does not work. Doing this by hand.
-chmod +x /usr/local/bin/scenario-status
-
-
-curl -X HEAD https://netnea.com/ping/katacoda-tutorial-2-intro -o /dev/null 2>/tmp/tmp.log
+curl -X HEAD https://netnea.com/ping/katacoda-tutorial-2-intro -o /dev/null 2>>/tmp/tmp.log
 
